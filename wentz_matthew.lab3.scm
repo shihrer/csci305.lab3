4dee8007b79b0a30d5ceaced4782b6ce0d75e6eb
matthewwentz <mattwentz1@sbcglobal.net>
2016-04-12 18:09:56 -0600

    Union

--- wentz_matthew.lab3.scm
+++ wentz_matthew.lab3.scm
@@ -1,32 +1,18 @@
-#!/usr/local/bin/guile -s
-!#
-
-(display "Hello World\n")
-
-;;; member? funtion
+(define (element? a lst)
+  (and (not (null? lst))
+       (or (eq? a (car lst))
+	   (element? a (cdr lst)))))
 
 
-;;;(define (member? e list)
-;;;	(cond((null? y) #f)
-;;;	((eqv? x (car y)) #t)
-;;;		(else (member? x (cdr y)))))
+(define (unionx list1 list2)
+	(if(null? list1) list2
+	(unionx (cdr list1)
+		(if(element? (car list1) list2)
+		list2
+		(cons(car list1) list2)))))
 
-
-(define (member? e list)
-	(if (memq e list))
-	#t
-	#f)
-
-(define (1 2 3 4))
-(member? 3 '(1 2 3 4))
-
-;;; union function
-
-(define (union list1 list2)
-	(cond ((null? list2) list1)
-	((element? (car list2) list1)
-	(union list1 (cdr list2)))
-	(#t (union (cons (car list2) list1) (cdr list2 ))))
-)
-
+(write (unionx '(blue eggs and cheese) '(cheese and blue eggs)))
+(newline)
+(write (unionx '(2 4) '(1 3 5)))
+(newline)
 
