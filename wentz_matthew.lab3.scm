b8208649a5c24ab2000fe5cbd9f74b99672a1f49
matthewwentz <mattwentz1@sbcglobal.net>
2016-04-12 21:29:53 -0600

    added comments

--- wentz_matthew.lab3.scm
+++ wentz_matthew.lab3.scm
@@ -1,9 +1,10 @@
+; element? function checks if an element exists in a list
 (define (element? a lst)
   (and (not (null? lst))
        (or (eq? a (car lst))
 	   (element? a (cdr lst)))))
-;;; union function
 
+; union function combines the values of two lists
 (define (union list1 list2)
 	(if(null? list1) list2
 	(union (cdr list1)
@@ -11,13 +12,13 @@
 		list2
 		(cons(car list1) list2)))))
 
-(write (union '(blue eggs and cheese) '(cheese and blue eggs)))
-(newline)
-(write (union '(2 4) '(1 3 5)))
+(write (union '(blue eggs and cheese) '(cheese and blue eggs))) ; returns 
+(newline)							; (cheese and blue eggs)
+(write (union '(2 4) '(1 3 5))) ; returns (4 2 1 3 5)
 (newline)
 
-;;; intersection function
-
+; intersect function compares two lists and returns
+; only the elements that are present in both
 (define (intersect list1 list2)
   (cond ((null? list1) '())((null? list2) '())
 	(else
@@ -25,9 +26,9 @@
 		  (if (element? (car list1) list2)
 		    (list (car list1))'())))))
 
-(write (intersect '(1 2 3 4 5) '(2 4)))
+(write (intersect '(1 2 3 4 5) '(2 4))) ; returns (4 2)
 (newline)
-(write (intersect '(blue moon) '(blue)))
+(write (intersect '(blue moon) '(blue))) ; returns (blue)
 (newline)
 
 
