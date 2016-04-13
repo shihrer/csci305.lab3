a08ad092e2adf4034269befe284b76ea82a4545d
Michael Shihrer <michael.shihrer@gmail.com>
2016-04-12 21:24:59 -0600

    member? and set? function completed

--- shihrer_michael.lab3.scm
+++ shihrer_michael.lab3.scm
@@ -1,21 +1,23 @@
 #!/usr/local/bin/guile -s
 !#
-; We named functions to avoid calling builtin functions.
 
-(define (member1? l list)				; Define the member function
-	(if (null? list) #f				; Return false if the argument is empty
-		(if (equal? l (car list)) #t		; Return true if 
-			(member1? l (cdr list)))))	; Call member1 function on
+(define (member? e list)				; Define member? function to accept an argument e and check if it exists in list. 
+	(if (null? list) #f				; Return false if the list is empty.  This means either no list was passed or the entire list was searched without finding a match.
+		(if (equal? e (car list)) #t		; Return true if argument i is equal to first item of someList, this means a match was found.
+			(member? e (cdr list)))))	; Call member? function recursively on the remaining items of someList (all but first item)
 
-(write (member1? 'one '(1 2 3 4 5)))
-(write (member1? 'd '(a b c d c b a)))
+(define (set? list)					; Define set? function to accept a list
+	(cond ((null? list) #t)				; Return true when list is null.  Means entire list has been checked or it's an empty list.
+		((member? (car list)			; Check to see if first item of list is a member of the rest of the list.
+			(cdr list)) #f)			; Return false if member? returns true.
+		(else (set? (cdr list)))))		; Otherwise, recursively call set? on remaining list.
 
-(define (set1? someList)
-	(cond ((null? someList) #f)
-		(else #t ))
 
-)
-
-(write (set1? '()))
-(write (set1? '(a b c d c b a)))
-;(write (set1? '(it was the best of times, it was the worst of times))
+; Tests
+(write (member? 'one '(1 2 3 4 5)))
+(write (member? 'd '(a b c d c b a)))
+(write (set? '()))
+(write (set? '(a b c d c b a)))
+(write (set? '(a)))
+(write (set? '(it was the best of times, it was the worst of times)))
+(write (set? '(this is a set)))
