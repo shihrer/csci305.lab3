c1a62bbf0d211be04a783142174f8db7f728d448
matthewwentz <mattwentz1@sbcglobal.net>
2016-04-12 21:01:35 -0600

    added the intersection function

--- wentz_matthew.lab3.scm
+++ wentz_matthew.lab3.scm
@@ -2,17 +2,34 @@
   (and (not (null? lst))
        (or (eq? a (car lst))
 	   (element? a (cdr lst)))))
+;;; union function
 
-
-(define (unionx list1 list2)
+(define (union list1 list2)
 	(if(null? list1) list2
-	(unionx (cdr list1)
+	(union (cdr list1)
 		(if(element? (car list1) list2)
 		list2
 		(cons(car list1) list2)))))
 
-(write (unionx '(blue eggs and cheese) '(cheese and blue eggs)))
+(write (union '(blue eggs and cheese) '(cheese and blue eggs)))
 (newline)
-(write (unionx '(2 4) '(1 3 5)))
+(write (union '(2 4) '(1 3 5)))
 (newline)
+
+;;; intersection function
+
+(define (intersect list1 list2)
+  (cond ((null? list1) '())((null? list2) '())
+	(else
+	  (append (intersect (cdr list1) list2)
+		  (if (element? (car list1) list2)
+		    (list (car list1))'())))))
+
+(write (intersect '(1 2 3 4 5) '(2 4)))
+(newline)
+(write (intersect '(blue moon) '(blue)))
+(newline)
+
+
+
 
